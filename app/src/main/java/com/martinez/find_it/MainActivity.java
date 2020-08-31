c5b80b51b5754f364b870e02b1178674c9af7bbe
Rayen <r.martinez.valenzuela00@gmail.com>
2020-08-31 12:46:14 -0400

    Segundo commit: se agregan widget y se generan las referencias desde java

--- app/src/main/java/com/martinez/find_it/MainActivity.java
+++ app/src/main/java/com/martinez/find_it/MainActivity.java
@@ -2,13 +2,25 @@ package com.martinez.find_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Creamos las variables que haran referencia a los widgets
+    private TextView txtTitulo;
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+        txtTitulo.setTextColor(Color.BLUE);
     }
 }
