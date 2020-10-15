8d17186e6cb3e67ed0b2ec2df0e5e8cb81beb6ea
Rayen <r.martinez.valenzuela00@gmail.com>
2020-10-14 21:39:34 -0300

    Creacion de dos nuevas Activitys, Integracion de Componentes y enlazamiento de estas

--- app/src/main/java/com/martinez/find_it/Usuario.java
+++ app/src/main/java/com/martinez/find_it/Usuario.java
@@ -2,7 +2,9 @@ package com.martinez.find_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +22,22 @@ public class Usuario extends AppCompatActivity {
         btnRegistrar = findViewById(R.id.btnRegistro);
         txtUsuario = findViewById(R.id.txtUsuario);
         txtPassword = findViewById(R.id.txtPassword);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentomap = new Intent(getApplicationContext(), MapaUsuario.class);
+                startActivity(intentomap);
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentoRegis = new Intent(getApplicationContext(), RegistroUsuario.class);
+                startActivity(intentoRegis);
+            }
+        });
     }
+
 }
